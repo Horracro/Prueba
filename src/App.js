e98a34e7d74908ef50acf11cb78cb77c2476ccf1
horracro <diegoherreratirado8@hotmail.com>
2019-10-13 23:48:15 -0500

    se avanzo el ejemplo           {e.description} Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,57 +1,82 @@
 import React from 'react';
 import './App.css';
+import tasks from './sample/tasks.json';
+import Tareas from './components/Tareas'
 
-/*const App = () => <div>Esto es verdad: <Hello/></div>
+console.log(tasks);
 
-class App extends react.Component{
-  render(){
-    return <div>Esto es verdad: <Hello/></div>
+class Hello extends React.Component {
+  state = { show: false }
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="holi" >
+          <h1>{this.props.text}</h1>
+          <Shrek burro="Shrek is love, Shrek is life" />
+          <button onClick={() => this.setState({ show: false })}>Ocultar contenido</button>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div>Hidden Quote
+       <button onClick={() =>  this.setState({ show: true }) }>Mostrar  contenido</button>
+        </div>
+
+      );
+    }
   }
-}*/
+}
+const LogoReact = () => <div><img id="react" src="./logo512.png" alt="shrek" /></div>
 
-/* function Hello(props){
-  return(
-    <div id="holi" >
-      <h1>{props.subtitle}</h1>
-     {props.text}
+function Shrek(burro) {
+  return (
+    <div>
+      <img id="shrk" src="./Shrek.png" alt="correr"></img>
+      <br />
+      <h1 id="love">{burro.burro}</h1>
+      <button onClick={function pres(){ alert(burro.burro);}}>Presiona </button>
     </div>
-  ); */
-class Hello extends React.Component{
-  state={
-    show: false
+  );
+}
+
+class Mapeo extends React.Component{
+  state = {
+    tasks:tasks
   }
-  
   render(){
-    if (this.state.show){
     return(
-  <div id="holi" >
-    <h1>{this.props.subtitle}</h1>
-     {this.props.text}
-      <button onClick={()=>this.setState({show:false})}>Cambiar estado</button>
-  </div>
+      <div>
+        {this.state.tasks.map(e=><p key={e.id}>
+          {e.title}<br/>
+          {e.description}<br/>
+          {e.done}
+        </p>)}
+      </div>
 
     );
   }
-  else {return(
-    <div>NO HAY ELEMENTOS
-      <button onClick={()=>this.setState({show: true})}>Cambiar estado</button>
-    </div>
-     
-  );
 }
-  }
-}
-
 
 function App() {
+ 
   return (
-   <div className="App">
-    HOLA MUNDO: <Hello text="Shrek es vida" subtitle="Shrek es amor"/> 
-    <Hello text="Paulo es mi hermano"/> 
-    <Hello text=" mi papa no me da dineros" />    
-    <img src="./logo192.png"/>
-   </div>
+    
+    <div className="App">
+      <h1 id="love">FRASES DEL CHUEK</h1>
+      <Hello text="Los Ogros son como cebollas" subtitle="Shrek es amor" />
+      <Hello text="Qué estas haciendo en mi pantano!?" subtitle="BOB ESPONJA" />
+      <Hello text="Deveritas Deveritas" />
+      <LogoReact />
+      <hr/>
+      <h1>PARTE 2</h1>
+     <Mapeo/>
+     <Tareas />
+     
+    </div>
+   
   );
 }
+
 
 export default App;
---
+++ src/components/Tareas.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Tareas extends Component{
+    render(){
+        return <h1>Soy una taza, una tetera</h1>
+    }
+
+
+}
+export default Tareas;
